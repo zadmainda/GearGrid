6264538ea8d30d1509cc2e5fb1253ab019766353
Zadock Mainda <kinara.zadock@gmail.com>
2024-02-13 11:43:43 +0300

    feat: add js for proudtc carousel functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ headerBarClose.addEventListener('click', () =>{
   headerBar.remove()
 })
 
+
+//HERO SLIDER
+
 const heroImgSlider = document.querySelector('.hero_slide');
 const heroArrows = document.querySelectorAll('.hero_arrow');
 let firstHeroImgWidth = document.querySelectorAll('.hero_slide img')[0].clientWidth;
@@ -16,6 +19,21 @@ heroArrows.forEach(arrow => {
       heroImgSlider.scrollLeft -= firstHeroImgWidth;
     }else{
       heroImgSlider.scrollLeft += firstHeroImgWidth;
+    }
+  });
+});
+
+// Product Carousel
+const productCarousel = document.querySelector('.carousel_track');
+const productArrows = document.querySelectorAll('.carousel_arrows');
+let firstProductWidth = document.querySelectorAll('.carousel_track .card')[0].clientWidth + 14.5;
+
+productArrows.forEach(arrow => {
+  arrow.addEventListener('click', () => {
+    if(arrow.id == "carousel_arrows-left"){
+      productCarousel.scrollLeft -= firstProductWidth;
+    }else{
+      productCarousel.scrollLeft += firstProductWidth;
     }
   });
 });
