60cb962f1a9343e95185003fd6daff16d4f4cbf0
Zadock Mainda <kinara.zadock@gmail.com>
2024-02-18 00:51:47 +0300

    fix: add guard clauses to home page  sliders JS logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,35 +7,82 @@ headerBarClose.addEventListener('click', () =>{
 
 
 //HERO SLIDER
+const heroSlider = () => {
+  const heroImgSlider = document.querySelector('.hero_slide');
+  const heroArrows = document.querySelectorAll('.hero_arrow');
+  
+  if (!heroImgSlider || !heroArrows ) {
+    return;
+  }
 
-const heroImgSlider = document.querySelector('.hero_slide');
-const heroArrows = document.querySelectorAll('.hero_arrow');
-let firstHeroImgWidth = document.querySelectorAll('.hero_slide img')[0].clientWidth;
+  const firstHeroImg = document.querySelectorAll('.hero_slide img');
 
+  if ( !firstHeroImg ) {
+    return;
+  }
 
-heroArrows.forEach(arrow => {
-  arrow.addEventListener('click', () => {
-    if(arrow.id == "hero_arrow-left"){
-      heroImgSlider.scrollLeft -= firstHeroImgWidth;
-    }else{
-      heroImgSlider.scrollLeft += firstHeroImgWidth;
-    }
+  const firstHeroImgWidth = document.querySelectorAll('.hero_slide img')[0].clientWidth;
+
+  if (heroImgSlider && heroArrows && firstHeroImgWidth) {
+    heroArrows.forEach(arrow => {
+      arrow.addEventListener('click', () => {
+        if (arrow.id == "hero_arrow-left") {
+          heroImgSlider.scrollLeft -= firstHeroImgWidth;
+        } else {
+          heroImgSlider.scrollLeft += firstHeroImgWidth;
+        }
+      });
+    });
+  } else {
+    return;
+  }
+}
+
+heroSlider()
+
+// landing Page Product Carousel
+
+const landingPageProductCarousel = () => {
+
+  const productCarousel = document.querySelector('.carousel_track');
+  const productArrows = document.querySelectorAll('.carousel_arrows');
+
+  if( !productCarousel || !productArrows) return;
+
+  firstCarouselCard = document.querySelectorAll('.carousel_track .card')[1];
+  if(!firstCarouselCard) return ;
+
+  let productMarginLeft = parseFloat(window.getComputedStyle(firstCarouselCard).marginLeft)
+  let firstProductWidth = firstCarouselCard.clientWidth + productMarginLeft;
+
+  productArrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      if (arrow.id == "carousel_arrows-left") {
+        productCarousel.scrollLeft -= firstProductWidth;
+      } else {
+        productCarousel.scrollLeft += firstProductWidth;
+      }
+    });
   });
-});
+}
 
-// Product Carousel
-const productCarousel = document.querySelector('.carousel_track');
-const productArrows = document.querySelectorAll('.carousel_arrows');
-let productMarginLeft = parseFloat(window.getComputedStyle(document.querySelectorAll('.carousel_track .card')[1]).marginLeft)
-let firstProductWidth = document.querySelectorAll('.carousel_track .card')[0].clientWidth + productMarginLeft;
+landingPageProductCarousel()
 
 
-productArrows.forEach(arrow => {
+// Product Template Page Main Slider 
+const productTemplateTrack = document.querySelector('.product_track');
+const productTemplateArrows = document.querySelectorAll('.product_arrow');
+let firstProductSlideWidth = document.querySelectorAll('.product_track img');
+
+console.log(firstProductSlideWidth)
+
+
+productTemplateArrows.forEach(arrow => {
   arrow.addEventListener('click', () => {
-    if(arrow.id == "carousel_arrows-left"){
-      productCarousel.scrollLeft -= firstProductWidth;
+    if(arrow.id == "product_arrow-left"){
+      productTemplateTrack.scrollLeft -= firstProductSlideWidth;
     }else{
-      productCarousel.scrollLeft += firstProductWidth;
+      productTemplateTrack.scrollLeft += firstProductSlideWidth;
     }
   });
 });
